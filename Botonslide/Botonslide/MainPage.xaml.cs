68040812753103b6116b993136f52d2edbe5cb1e
Dariodroid <53498306+Dariodroid@users.noreply.github.com>
2022-06-06 13:22:16 -0500

    Agregar archivos de proyecto.

---
+++ Botonslide/Botonslide/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Botonslide
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Handle_SlideCompleted(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Mensaje", "Completado", "Ok");
+        }
+    }
+}
